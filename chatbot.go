857bea0eb987de8ec2fdd41fc4ed996554d66c67
Siji Surendranadh C <sijisurendranadhc@Sijis-MacBook-Pro.local>
2021-10-08 13:07:19 +0530

    adding emailtopdf converter

---
+++ chatbot.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	"github.com/go-chat-bot/bot/slack"
+	_ "github.com/go-chat-bot/plugins/catfacts"
+	_ "github.com/go-chat-bot/plugins/catgif"
+	_ "github.com/go-chat-bot/plugins/chucknorris"
+	// Import all the commands you wish to use
+)
+
+func main() {
+	slack.Run(os.Getenv("SLACK_TOKEN"))
+}
