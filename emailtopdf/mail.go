35b3dc88d14831ec5d0e8f4dbb01623dbc4b4c9c
sijisurendranadh <sijisurendranadh@gmail.com>
2021-10-08 13:41:57 +0530

    Update mail.go
    Removed unused function

--- emailtopdf/mail.go
+++ emailtopdf/mail.go
@@ -97,25 +97,3 @@ func checkerr(err error, msg string) {
 	}
 }
 
-func GeneratePdf(filename string) error {
-
-	pdf := gofpdf.New("P", "mm", "A4", "")
-	pdf.AddPage()
-	pdf.SetFont("Arial", "B", 16)
-
-	// CellFormat(width, height, text, border, position after, align, fill, link, linkStr)
-	pdf.CellFormat(190, 7, "Welcome to golangcode.com", "0", 0, "CM", false, 0, "")
-
-	// ImageOptions(src, x, y, width, height, flow, options, link, linkStr)
-	//pdf.ImageOptions(
-	//    "avatar.jpg",
-	//    80, 20,
-	//    0, 0,
-	//    false,
-	//    gofpdf.ImageOptions{ImageType: "JPG", ReadDpi: true},
-	//    0,
-	//    "",
-	// )
-
-	return pdf.OutputFileAndClose(filename)
-}
